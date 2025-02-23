e44819bb1057babcb839a4574f62d1d48ecdc3ed
MightyBooshUK <Odawson@gmail.com>
2025-02-23 00:02:37 +0000

    Added dynamic loading code to question game

--- scripts/update_question_text.gd
+++ scripts/update_question_text.gd
@@ -1,24 +1,50 @@
 extends MiniGame
 @onready var preload_input_box = preload("res://scenes/QuestionGame/InputTextbox.tscn")
+var answers_sprites: Array
 var input_box_instance: Control
 var question_string: String
 var answer_string: String
 
+var dict_questions = {
+	"What is the Capital of France":"Paris",
+	"What animal goes moo":["Cow", "Pig", "Dog"],
+	"":""
+}
+
 func _ready() -> void:
 	tmr_time = 2.5
+	preload_answers()
 	super._ready()
 	m_type = MiniGameType.QUESTION
-	#Spawn an Input Textbox by adding it to parent as a sibling
-	#link the signal "input_event_end"
-	#when that signal is recieved emit the mini_game_base signal 
+	
+	initialise_inputbox()
+	
+
+
+func initialise_inputbox() -> void:
 	input_box_instance = preload_input_box.instantiate()
 	input_box_instance.input_event_end.connect(_question_end)
 	input_box_instance.position = Vector2(50,50)
 	input_box_instance.target_answer = answer_string
-	
 	get_parent().add_child(input_box_instance)
+
+func preload_answers() -> void:
+	var i = randi_range(0,1)
+	match i:
+		0:
+			question_string = "What animal goes moo?"
+			answer_string = "pig"
+			answers_sprites = [preload("res://Assets/Sprites/answer_sprites/Answer Pig.png"), preload("res://Assets/Sprites/answer_sprites/answer_cow.png"), preload("res://Assets/Sprites/answer_sprites/answer_dog.png")]
+		1:
+			pass
 	pass
 
+func set_answers() -> void:
+	get_child(2).get_child(0).texture = answers_sprites[0].instantiate()
+	get_child(3).get_child(0).texture = answers_sprites[1].instantiate()
+	get_child(4).get_child(0).texture = answers_sprites[2].instantiate()
+	pass
+	
 # Called when the node enters the scene tree for the first time.
 func _set_text(temp_string: String ) -> void:
 	$RichTextLabel.set_text(temp_string)
